3f11bcdb7484a62168712f9352523c53d5b4921b
gianlucamazzeo <gianlucamazzeo@gmail.com>
2019-09-18 10:09:13 +0200

    add song list to reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songReducer = () => {
+    return [
+        { title: 'No scrubs', duration: '4.05' },
+        { title: 'Macarena', duration: '2.35' },
+        { title: 'All Star', duration: '3.35' },
+        { title: 'I Want it that way', duration: '1.35' },
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if(action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    return selectedSong;
+}
