b64d5e90bdafbf886d9c7ea01fe73a5f87f84e0b
gianlucamazzeo <gianlucamazzeo@gmail.com>
2019-09-18 11:34:08 +0200

    add SongList.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import SongList from './SongList';
 
 const App = () => {   ////  creato come componente funzionale
-
-    return <div>App</div>
-
+    return (
+        <div>
+           <SongList />
+        </div>
+    );
 };
 
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
 
-const songReducer = () => {
+const songsReducer = () => {
     return [
         { title: 'No scrubs', duration: '4.05' },
         { title: 'Macarena', duration: '2.35' },
